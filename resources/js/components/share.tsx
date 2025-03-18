8f6ffda056467e4f1f65e6b3469ef2f1ba0560b5
Sean Fisher <srtfisher@gmail.com>
2025-03-18 17:00:29 -0400

    Select text on focus

--- resources/js/components/share.tsx
+++ resources/js/components/share.tsx
@@ -18,22 +18,32 @@ export function SharePopover() {
         <div aria-live="assertive" className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
             <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
                 <Transition show={show}>
-                    <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black/5 transition data-[closed]:opacity-0 data-[enter]:transform data-[enter]:duration-300 data-[enter]:ease-out data-[closed]:data-[enter]:translate-y-2 data-[leave]:duration-100 data-[leave]:ease-in data-[closed]:data-[enter]:sm:translate-x-2 data-[closed]:data-[enter]:sm:translate-y-0 dark:bg-gray-700 dark:ring-gray-900/20">
+                    <div className="
+                        pointer-events-auto
+                        w-full max-w-sm overflow-hidden
+                        bg-white dark:bg-gray-400 dark:ring-gray-900/20
+                        rounded-lg
+                        shadow-lg ring-1 ring-black/5 transition
+                        data-[closed]:opacity-0 data-[enter]:transform data-[enter]:duration-300 data-[enter]:ease-out data-[closed]:data-[enter]:translate-y-2 data-[leave]:duration-100 data-[leave]:ease-in data-[closed]:data-[enter]:sm:translate-x-2 data-[closed]:data-[enter]:sm:translate-y-0 ">
                         <div className="p-4">
                             <div className="flex items-start">
                                 <div className="ml-3 w-0 flex-1 pt-0.5">
-                                    <p className="text-sm font-medium text-gray-900 dark:text-white">Share created!</p>
+                                    <p className="text-sm font-medium text-gray-900 dark:text-gray-800">Share created!</p>
                                     <div className="mt-3 flex space-x-7">
                                         <input
-                                            autoFocus
+                                            // Select the text on focus
+                                            onFocus={(e) => {
+                                                e.target.select();
+                                                e.target.setSelectionRange(0, e.target.value.length);
+                                            }}
                                             readOnly
                                             type="text"
                                             value={shareUrl}
-                                            className="w-full rounded-md border-gray-300 bg-gray-50 px-2 py-1 text-sm text-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-300"
+                                            className="w-full rounded-md border-gray-300 bg-gray-50 px-2 py-1 text-sm text-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-200"
                                         />
                                         <button
                                             type="button"
-                                            className="rounded-md bg-white text-sm font-medium text-blue-600 hover:text-blue-500 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none dark:bg-transparent dark:text-gray-100"
+                                            className="rounded-md bg-white text-sm font-medium text-blue-600 hover:text-blue-500 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none dark:bg-transparent dark:text-gray-700"
                                             onClick={() => {
                                                 navigator.clipboard.writeText(shareUrl).then(() => {
                                                     setCopied(true);
@@ -51,7 +61,7 @@ export function SharePopover() {
                                         onClick={() => {
                                             setShow(false);
                                         }}
-                                        className="inline-flex cursor-pointer rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none dark:bg-gray-700 dark:text-gray-500 dark:hover:text-gray-300"
+                                        className="inline-flex cursor-pointer rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none dark:bg-gray-400 dark:text-gray-600 dark:hover:text-gray-300"
                                     >
                                         <span className="sr-only">Close</span>
                                         <XMarkIcon aria-hidden="true" className="size-5" />

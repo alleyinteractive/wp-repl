3c8ba746e9c5d79a90b23ca92f10928e21813f6c
Sean Fisher <srtfisher@gmail.com>
2025-03-18 20:54:56 -0400

    Unset middleware change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,8 @@ use App\Http\Middleware\RemoveCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
